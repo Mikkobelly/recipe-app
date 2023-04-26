6591613483f5ff3eb54cafefc855393eddf1ba5b
Mikkobelly <mikiakuta0529@gmail.com>
2023-04-26 17:41:32 +0100

    Created login_view, logout_view and according templates

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import recipe_home
-from .views import RecipeListView, RecipeDetailView
+from .views import recipe_home, RecipeListView, RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', recipe_home),
-    path('recipes/', RecipeListView.as_view(), name='recipes'),
+    path('recipes/', RecipeListView.as_view(), name='list'),
     path('recipes/<pk>', RecipeDetailView.as_view(), name='detail')
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 # Create your views here.
 def recipe_home(request):
     return render(request, 'recipes/recipe_home.html')
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
